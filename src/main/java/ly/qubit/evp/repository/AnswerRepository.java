197d93ae423d9e99caae31cb9b5303fd4707170a
ayuob <ayuob.it@gmail.com>
2024-06-03 23:01:19 +0200

    add Answer model with repo service and REST layers

---
+++ src/main/java/ly/qubit/evp/repository/AnswerRepository.java
@@ -0,0 +1,6 @@
+package ly.qubit.evp.repository;
+
+import ly.qubit.evp.domain.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {}
